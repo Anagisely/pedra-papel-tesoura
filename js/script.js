54b1fb443c69987bcb95776bfc8ad06cadf8614d
Ana Gisely <anagisely.silva.alves@gmail.com>
2020-07-31 23:08:45 -0300

    javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,106 @@
-let user = A;
-let computer = A;
+/*0 é pedra
+  1 é papel
+  2 é tesoura */ 
 
-if (user == 1 && computer == 2) {
+
+ const rock= document.getElementById("rock")
+  const paper= document.getElementById("paper")
+  const scissors= document.getElementById("scissors")
+rock.addEventListener('click', (e)=>{
+   let pedra = e.path[1].value
+   while(pedra==0){
+    let computer = parseInt(Math.random()*(3-0)-0)
     
-}if (user == 1 && computer == 3) {
-    
-}if (user == 1 && computer == 3) {
-    
-} else {
+   if(pedra==computer){
+       console.log("EMPATE!!")
+
+   }if(computer==1){
+        console.log("você perdeu")
+   }if(computer==2){
+       console.log("você ganhou!!!")
+
+    }
+    pedra++
     
 }
+})
+paper.addEventListener('click', (e)=>{
+    let papel = e.path[1].value
+   while(papel==1){
+    let computer = parseInt(Math.random()*(3-0)-0)
+   if(papel==computer){
+       console.log("EMPATE!!")
+
+   }if(computer==0){
+        console.log("você ganhou!!")
+   }if(computer==2){
+       console.log("você perdeu")
+   }
+   papel++
+}
+})
+scissors.addEventListener('click', (e)=>{
+    let tesoura = e.path[1].value
+   while(tesoura==2){
+    let computer = parseInt(Math.random()*(3-0)-0)
+   if(tesoura==computer){
+       console.log("EMPATE!!")
+
+   }if(computer==0){
+        console.log("você perdeu")
+   }if(computer==1){
+       console.log("você ganhou!!!")
+   }
+   tesoura++
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+let computer = parseInt(Math.random()*3)
+console.log(computer)
+
+
+
+    console.log(`placar usuario é ${placarUser} e placar computer é ${placarComputer}`)
+    if(computer== 0 && user ==1){
+        console.log( "usuario venceu")
+        placarUser++
+    }else if (computer==1 && user==2){
+        console.log( "usuario venceu")
+        placaruser++ 
+    }else if(computer==2 && user==0){
+        console.log( "usuario venceu")
+        placarUser++
+    }
+    else if (computer==2 && user==1){
+        console.log( "computador venceu")
+        placarComputer++ 
+    }else if(computer==0 && user==2){
+        console.log( "computador venceu")
+        placarComputer++
+    }else if(computer == 1 && user==0){
+        console.log( "computador venceu")
+        placarComputer++
+    }else if(){
+        console.log("empate")
+    }*/
